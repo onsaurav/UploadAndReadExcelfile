d3dcc9cc4e3ca6585900067f44c341fdf0923a01
Kartik Chandra Biswas <onsaurav@hotmail.com>
2019-04-21 21:58:54 +0600

    Initial commit

---
+++ UploadExcel/Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="UploadExcel._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:FileUpload ID="FileUploadToServer" Width="300px" runat="server" />
+        <asp:Button ID="btnUpload" runat="server" Text="Upload File" OnClick="btnUpload_Click"
+            ValidationGroup="vg" style="width: 99px" />
+        <br />
+        <br />
+        <asp:Label ID="lblMsg" runat="server" ForeColor="Green" Text=""></asp:Label>
+        <br />
+        <asp:GridView ID="GridView1" runat="server" EmptyDataText="No record found!" 
+            Height="25px">
+            <RowStyle Width="175px" />
+            <EmptyDataRowStyle BackColor="Silver" BorderColor="#999999" BorderStyle="Solid" 
+                BorderWidth="1px" ForeColor="#003300" />
+            <HeaderStyle BackColor="#6699FF" BorderColor="#333333" BorderStyle="Solid" 
+                BorderWidth="1px" VerticalAlign="Top" Width="200px"  Wrap="True" />
+              
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ UploadExcel/Default.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Data.OleDb;
+
+
+namespace UploadExcel
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+       
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            //Get path from web.config file to upload
+            string FilePath = ConfigurationManager.AppSettings["FilePath"].ToString();
+            string filename = string.Empty;
+            //To check whether file is selected or not to uplaod
+            if (FileUploadToServer.HasFile)
+            {
+                try
+                {
+                    string[] allowdFile = { ".xls", ".xlsx" };
+                    //Here we are allowing only excel file so verifying selected file pdf or not
+                    string FileExt = System.IO.Path.GetExtension(FileUploadToServer.PostedFile.FileName);
+                    //Check whether selected file is valid extension or not
+                    bool isValidFile = allowdFile.Contains(FileExt);
+                    if (!isValidFile)
+                    {
+                        lblMsg.ForeColor = System.Drawing.Color.Red;
+                        lblMsg.Text = "Please upload only Excel";
+                    }
+                    else
+                    {
+                        // Get size of uploaded file, here restricting size of file
+                        int FileSize = FileUploadToServer.PostedFile.ContentLength;
+                        if (FileSize <= 1048576)//1048576 byte = 1MB
+                        {
+                            //Get file name of selected file
+                            filename = Path.GetFileName(Server.MapPath(FileUploadToServer.FileName));
+                            
+                            //Save selected file into server location
+                            FileUploadToServer.SaveAs(Server.MapPath(FilePath) + filename);
+                            //Get file path
+                            string filePath = Server.MapPath(FilePath) + filename;
+                            //Open the connection with excel file based on excel version
+                            OleDbConnection con = null;
+                            if (FileExt == ".xls")
+                            {
+                                con = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filePath + ";Extended Properties=Excel 8.0;");
+                            
+                            }
+                            else if (FileExt == ".xlsx")
+                            {
+                                con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filePath + ";Extended Properties=Excel 12.0;");
+                            }
+                            con.Open();
+                            //Get the list of sheet available in excel sheet
+                            DataTable dt = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                            //Get first sheet name
+                            string getExcelSheetName = dt.Rows[0]["Table_Name"].ToString();
+                            //Select rows from first sheet in excel sheet and fill into dataset
+                            OleDbCommand ExcelCommand = new OleDbCommand(@"SELECT * FROM [" + getExcelSheetName + @"]", con);
+                            OleDbDataAdapter ExcelAdapter = new OleDbDataAdapter(ExcelCommand);
+                            DataSet ExcelDataSet = new DataSet();
+                            ExcelAdapter.Fill(ExcelDataSet);
+                            con.Close();
+                            //Bind the dataset into gridview to display excel contents
+                            GridView1.DataSource = ExcelDataSet;
+                            GridView1.DataBind();
+                        }
+                        else
+                        {
+                            lblMsg.Text = "Attachment file size should not be greater then 1 MB!";
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    lblMsg.Text = "Error occurred while uploading a file: " + ex.Message;
+                }
+            }
+            else
+            {
+                lblMsg.Text = "Please select a file to upload.";
+            }
+        }
+    }
+}
